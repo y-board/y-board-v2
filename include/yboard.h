bc47aad05d0303ba98fae043655b45310c0a2849
Philip Lundrigan <lundrigan@byu.edu>
2024-06-13 09:29:00 -0700

    Add documentation

--- include/yboard.h
+++ include/yboard.h
@@ -9,13 +9,37 @@ class YBoardV2 {
     YBoardV2();
     virtual ~YBoardV2();
 
+    /*
+     *  This function initializes the YBoard. This function must be called before using any of the
+     * YBoard features.
+     */
     void setup();
 
     ////////////////////////////// LEDs ///////////////////////////////////////////
+    /*
+     *  This function sets the color of an individual LED.
+     *  The index is an integer between 1 and 20, representing the number of the
+     * target LED (for example, 1 corresponds to the first LED on the board).
+     *  The red, green, and blue values are integers between 0 and 255, representing
+     * the intensity of the corresponding color. For example, if you want to set the
+     * LED to red, you would set red to 255 and green and blue to 0.
+     */
     void set_led_color(uint16_t index, uint8_t red, uint8_t green, uint8_t blue);
 
+    /*
+     *  This function sets the brightness of all the LEDs on the board.
+     *  The brightness is an integer between 0 and 255, representing the intensity
+     * of the LEDs. A brightness of 0 is off, and a brightness of 255 is full
+     * brightness.
+     */
     void set_led_brightness(uint8_t brightness);
 
+    /*
+     *  This function sets the color of all the LEDs on the board.
+     *  The red, green, and blue values are integers between 0 and 255, representing
+     * the intensity of the corresponding color. For example, if you want to set all
+     * the LEDs to red, you would set red to 255 and green and blue to 0.
+     */
     void set_all_leds_color(uint8_t red, uint8_t green, uint8_t blue);
 
     ////////////////////////////// Switches/Buttons ///////////////////////////////
@@ -39,9 +63,22 @@ class YBoardV2 {
      */
     bool get_button(uint8_t button_idx);
 
+    /*
+     *  This function returns the value of the knob.
+     *  The return type is an integer between 0 and 100, representing the position
+     * of the knob. A value of 0 corresponds to the knob being turned all the way to
+     * the left, and a value of 100 corresponds to the knob being turned all the way
+     * to the right.
+     */
     int get_knob();
 
     ////////////////////////////// Speaker/Tones //////////////////////////////////
+    /*
+     *  This function plays a note on the speaker.
+     *  The freq parameter is an integer representing the frequency of the note to be
+     * played. The duration parameter is a long integer representing the duration of
+     * the note in milliseconds.
+     */
     void play_note(unsigned int freq, unsigned long duration);
 
     // LEDs
